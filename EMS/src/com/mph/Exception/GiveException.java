149f48c8fa12df35a03f3ce148af0958803d9ee9
Vinuthana <cvinuthana@gmail.com>
2021-04-28 14:51:32 +0530

    done

---
+++ EMS/src/com/mph/Exception/GiveException.java
@@ -0,0 +1,16 @@
+package com.mph.Exception;
+
+public class GiveException {
+
+	public GiveException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "UserNotFound. Plz check the Credential";
+	}
+
+	
+}
---
+++ EMS/src/com/mph/Exception/UserNotFoundException.java
@@ -0,0 +1,19 @@
+package com.mph.Exception;
+
+public class UserNotFoundException {
+
+	public UserNotFoundException() {
+		
+	}
+
+	@Override
+	public String toString() {
+		return "UserNotFoundException []";
+	}
+
+	public void UserNotFoundException() {
+		System.out.println("From User Not Found Exception ");
+		
+	}
+
+}
